f0fd3809dc81c465deb6d2c8f73e5ef24c9b653e
janbarari <mehdimjt@gmail.com>
2022-07-26 19:23:56 +0430

    RenderTotalBuildReportStage.kt refactored

--- src/main/kotlin/io/github/janbarari/gradle/analytics/metric/totalbuild/report/RenderTotalBuildReportStage.kt
+++ src/main/kotlin/io/github/janbarari/gradle/analytics/metric/totalbuild/report/RenderTotalBuildReportStage.kt
@@ -23,13 +23,17 @@
 package io.github.janbarari.gradle.analytics.metric.totalbuild.report
 
 import io.github.janbarari.gradle.analytics.domain.model.report.Report
+import io.github.janbarari.gradle.analytics.metric.initialization.report.RenderInitializationReportStage
 import io.github.janbarari.gradle.core.Stage
 import io.github.janbarari.gradle.extension.ensureNotNull
 import io.github.janbarari.gradle.extension.getTextResourceContent
 import io.github.janbarari.gradle.extension.isNull
 import io.github.janbarari.gradle.extension.removeLastChar
+import io.github.janbarari.gradle.extension.toArrayString
 import io.github.janbarari.gradle.extension.toIntList
 import io.github.janbarari.gradle.extension.whenEach
+import io.github.janbarari.gradle.extension.whenNotNull
+import io.github.janbarari.gradle.utils.HtmlUtils
 import io.github.janbarari.gradle.utils.MathUtils
 
 class RenderTotalBuildReportStage(
@@ -37,41 +41,42 @@ class RenderTotalBuildReportStage(
 ): Stage<String, String> {
 
     companion object {
-        private const val CHART_EMPTY_POSITION_RATE = 30
+        private const val CHART_SUGGESTED_MIN_MAX_PERCENTAGE = 30
+        private const val TOTALBUILD_METRIC_TEMPLATE_ID = "%totalbuild-metric%"
+        private const val TOTALBUILD_METRIC_TEMPLATE_FILE_NAME = "totalbuild-metric-template"
     }
 
     override suspend fun process(input: String): String {
         if (report.totalBuildReport.isNull())
-            return input.replace("%totalbuild-metric%",
-                "<p>Total Build Median Chart is not available!</p><div class=\"space\"></div>")
+            return input.replace(TOTALBUILD_METRIC_TEMPLATE_ID, getEmptyRender())
 
-        val values = ensureNotNull(report.totalBuildReport).values.map { it.value }.toIntList()
+        return input.replace(TOTALBUILD_METRIC_TEMPLATE_ID, getMetricRender())
+    }
 
-        val labels = StringBuilder()
-        ensureNotNull(report.totalBuildReport).values.map { it.description }.whenEach {
-            labels.append("\"$this\"").append(",")
+    fun getEmptyRender(): String {
+        return HtmlUtils.renderMessage("Total build chart is not available!")
+    }
+
+    fun getMetricRender(): String {
+        var renderedTemplate = HtmlUtils.getTemplate(TOTALBUILD_METRIC_TEMPLATE_FILE_NAME)
+        report.totalBuildReport.whenNotNull {
+            val chartValues = values.map { it.value }
+                .toIntList()
+                .toString()
+
+            val chartLabels = values.map { it.description }
+                .toArrayString()
+
+            val chartSuggestedMaxValue = MathUtils.sumWithPercentage(maxValue, CHART_SUGGESTED_MIN_MAX_PERCENTAGE)
+            val chartSuggestedMinValue = MathUtils.deductWithPercentage(minValue, CHART_SUGGESTED_MIN_MAX_PERCENTAGE)
+
+            renderedTemplate = renderedTemplate
+                .replace("%totalbuild-suggested-max-value%", chartSuggestedMaxValue.toString())
+                .replace("%totalbuild-suggested-min-value%", chartSuggestedMinValue.toString())
+                .replace("%totalbuild-median-values%", chartValues)
+                .replace("%totalbuild-median-labels%", chartLabels)
         }
-        // because the last item should not have ',' separator.
-        labels.removeLastChar()
-
-        val chartMaxValue = MathUtils.sumWithPercentage(
-            ensureNotNull(report.totalBuildReport).maxValue,
-            CHART_EMPTY_POSITION_RATE
-        )
-        val chartMinValue = MathUtils.deductWithPercentage(
-            ensureNotNull(report.totalBuildReport).minValue,
-            CHART_EMPTY_POSITION_RATE
-        )
-
-        var template = getTextResourceContent("totalbuild-metric-template.html")
-
-        template = template
-            .replace("%totalbuild-max-value%", chartMaxValue.toString())
-            .replace("%totalbuild-min-value%", chartMinValue.toString())
-            .replace("%totalbuild-median-values%", values.toString())
-            .replace("%totalbuild-median-labels%", labels.toString())
-
-        return input.replace("%totalbuild-metric%", template)
+        return renderedTemplate
     }
 
 }
