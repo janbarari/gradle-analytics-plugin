305b96048e8f9d6d38bb9aae0acd52727c46998f
janbarari <mehdimjt@gmail.com>
2022-08-12 12:38:50 +0300

    refactor: Report class children structure test: CreateCacheHitReportStageTest

--- src/main/kotlin/io/github/janbarari/gradle/analytics/domain/model/report/Report.kt
+++ src/main/kotlin/io/github/janbarari/gradle/analytics/domain/model/report/Report.kt
@@ -22,35 +22,38 @@
  */
 package io.github.janbarari.gradle.analytics.domain.model.report
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 
 @JsonClass(generateAdapter = true)
 data class Report(
+    @Json(name = "branch")
     val branch: String,
-    val requestedTasks: String
-) : java.io.Serializable {
-
-    var initializationProcessReport: InitializationProcessReport? = null
-
-    var configurationProcessReport: ConfigurationProcessReport? = null
-
-    var executionProcessReport: ExecutionProcessReport? = null
-
-    var overallBuildProcessReport: OverallBuildProcessReport? = null
-
-    var modulesSourceCountReport: ModulesSourceCountReport? = null
-
-    var modulesMethodCountReport: ModulesMethodCountReport? = null
-
-    var cacheHitReport: CacheHitReport? = null
-
-    var successBuildRateReport: SuccessBuildRateReport? = null
-
-    var dependencyResolveProcessReport: DependencyResolveProcessReport? = null
-
+    @Json(name = "requested_tasks")
+    val requestedTasks: String,
+    @Json(name = "initialization_process_report")
+    var initializationProcessReport: InitializationProcessReport? = null,
+    @Json(name = "configuration_process_report")
+    var configurationProcessReport: ConfigurationProcessReport? = null,
+    @Json(name = "execution_process_report")
+    var executionProcessReport: ExecutionProcessReport? = null,
+    @Json(name = "overall_build_process_report")
+    var overallBuildProcessReport: OverallBuildProcessReport? = null,
+    @Json(name = "modules_source_count_report")
+    var modulesSourceCountReport: ModulesSourceCountReport? = null,
+    @Json(name = "modules_method_count_report")
+    var modulesMethodCountReport: ModulesMethodCountReport? = null,
+    @Json(name = "cache_hit_report")
+    var cacheHitReport: CacheHitReport? = null,
+    @Json(name = "success_build_rate_report")
+    var successBuildRateReport: SuccessBuildRateReport? = null,
+    @Json(name = "dependency_resolve_process_report")
+    var dependencyResolveProcessReport: DependencyResolveProcessReport? = null,
+    @Json(name = "parallel_execution_rate_report")
     var parallelExecutionRateReport: ParallelExecutionRateReport? = null
+) : java.io.Serializable {
 
     fun toJson(): String {
         val moshi: Moshi = Moshi.Builder().build()
