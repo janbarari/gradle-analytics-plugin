c0d9dc2c1234db47043404c4e1e9d5679b20b203
janbarari <mehdimjt@gmail.com>
2022-11-18 23:07:54 +0330

    Refactor StringExtensionsTest.kt

--- src/test/kotlin/io/github/janbarari/gradle/extension/StringExtensionsTest.kt
+++ src/test/kotlin/io/github/janbarari/gradle/extension/StringExtensionsTest.kt
@@ -29,12 +29,12 @@ class StringExtensionsTest {
 
     @Test
     fun `check toFilePath() returns correct path`() {
-        val path1 = "/Users/spider/project/namoos/"
-        assertEquals("/Users/spider/project/namoos", path1.toRealPath())
-        val path2 = "/Users/spider/project/namoos//"
-        assertEquals("/Users/spider/project/namoos", path2.toRealPath())
-        val path3 = "/Users/spider/project/namoos///"
-        assertEquals("/Users/spider/project/namoos", path3.toRealPath())
+        val path1 = "/Users/zan/zendegi/azadi/"
+        assertEquals("/Users/zan/zendegi/azadi", path1.toRealPath())
+        val path2 = "/Users/zan/zendegi/azadi//"
+        assertEquals("/Users/zan/zendegi/azadi", path2.toRealPath())
+        val path3 = "/Users/zan/zendegi/azadi///"
+        assertEquals("/Users/zan/zendegi/azadi", path3.toRealPath())
     }
 
     @Test
