4d0d5e5d5e8a2d134ea5eb75b541d51dfc9d0330
janbarari <mehdimjt@gmail.com>
2022-11-20 18:56:58 +0330

    Add RenderModulesSourceSizeReportStageTest.kt

--- src/test/kotlin/io/github/janbarari/gradle/analytics/metric/modulessourcesize/report/RenderModulesSourceSizeReportStageTest.kt
+++ src/test/kotlin/io/github/janbarari/gradle/analytics/metric/modulessourcesize/report/RenderModulesSourceSizeReportStageTest.kt
@@ -22,10 +22,12 @@
  */
 package io.github.janbarari.gradle.analytics.metric.modulessourcesize.report
 
+import io.github.janbarari.gradle.analytics.domain.model.report.ModulesSourceSizeReport
 import io.github.janbarari.gradle.analytics.domain.model.report.Report
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class RenderModulesSourceSizeReportStageTest {
 
@@ -39,6 +41,219 @@ class RenderModulesSourceSizeReportStageTest {
 
         val expectedAnswer = "<p>Modules Source Size is not available!</p><div class=\"space\"></div>"
         assertEquals(expectedAnswer, result)
+    }
+
+    @Test
+    fun `check render when total diff rate is positive`() = runBlocking {
+        val report = Report("main", "assemble")
+        report.modulesSourceSizeReport = ModulesSourceSizeReport(
+            listOf(
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":woman",
+                    sizeInKb = 56,
+                    coverageRate = 14F,
+                    diffRate = 12F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":life",
+                    sizeInKb = 100,
+                    coverageRate = 23F,
+                    diffRate = 0F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":freedom",
+                    sizeInKb = 112,
+                    coverageRate = 65F,
+                    diffRate = -14F
+                )
+            ),
+            totalSourceSizeInKb = 1024,
+            totalDiffRate = 12F
+        )
+
+        val renderTemplate = "%modules-source-size-metric%"
+        val stage = RenderModulesSourceSizeReportStage(report)
+        val result = stage.process(renderTemplate)
+
+        assertTrue {
+            result.contains("<tr>\n" +
+                    "                        <td colspan=\"2\">Total</td>\n" +
+                    "                        <td>1024kb</td>\n" +
+                    "                        <td>Σ</td>\n" +
+                    "                        <td>+12.0%</td>\n" +
+                    "                    </tr>")
+        }
+        assertTrue {
+            result.contains("labels: [\":woman\", \":life\", \":freedom\"]")
+        }
+        assertTrue {
+            result.contains("var _data = [56, 100, 112];")
+        }
+        assertTrue {
+            result.contains("<td>1</td>\n" +
+                    "    <td>:woman</td>\n" +
+                    "    <td>56kb</td>\n" +
+                    "    <td>14.0%</td>\n" +
+                    "    <td>+12.0%</td>")
+        }
+        assertTrue {
+            result.contains("<td>2</td>\n" +
+                    "    <td>:life</td>\n" +
+                    "    <td>100kb</td>\n" +
+                    "    <td>23.0%</td>\n" +
+                    "    <td>Equals</td>")
+        }
+        assertTrue {
+            result.contains(
+                "<td>3</td>\n" +
+                        "    <td>:freedom</td>\n" +
+                        "    <td>112kb</td>\n" +
+                        "    <td>65.0%</td>\n" +
+                        "    <td>-14.0%</td>"
+            )
+        }
+    }
+
+    @Test
+    fun `check render when total diff rate is zero`() = runBlocking {
+        val report = Report("main", "assemble")
+        report.modulesSourceSizeReport = ModulesSourceSizeReport(
+            listOf(
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":woman",
+                    sizeInKb = 56,
+                    coverageRate = 14F,
+                    diffRate = 12F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":life",
+                    sizeInKb = 100,
+                    coverageRate = 23F,
+                    diffRate = 0F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":freedom",
+                    sizeInKb = 112,
+                    coverageRate = 65F,
+                    diffRate = -14F
+                )
+            ),
+            totalSourceSizeInKb = 1024,
+            totalDiffRate = 0F
+        )
+
+        val renderTemplate = "%modules-source-size-metric%"
+        val stage = RenderModulesSourceSizeReportStage(report)
+        val result = stage.process(renderTemplate)
+
+        assertTrue {
+            result.contains("<tr>\n" +
+                    "                        <td colspan=\"2\">Total</td>\n" +
+                    "                        <td>1024kb</td>\n" +
+                    "                        <td>Σ</td>\n" +
+                    "                        <td>Equals</td>\n" +
+                    "                    </tr>")
+        }
+        assertTrue {
+            result.contains("labels: [\":woman\", \":life\", \":freedom\"]")
+        }
+        assertTrue {
+            result.contains("var _data = [56, 100, 112];")
+        }
+        assertTrue {
+            result.contains("<td>1</td>\n" +
+                    "    <td>:woman</td>\n" +
+                    "    <td>56kb</td>\n" +
+                    "    <td>14.0%</td>\n" +
+                    "    <td>+12.0%</td>")
+        }
+        assertTrue {
+            result.contains("<td>2</td>\n" +
+                    "    <td>:life</td>\n" +
+                    "    <td>100kb</td>\n" +
+                    "    <td>23.0%</td>\n" +
+                    "    <td>Equals</td>")
+        }
+        assertTrue {
+            result.contains(
+                "<td>3</td>\n" +
+                        "    <td>:freedom</td>\n" +
+                        "    <td>112kb</td>\n" +
+                        "    <td>65.0%</td>\n" +
+                        "    <td>-14.0%</td>"
+            )
+        }
+    }
+
+    @Test
+    fun `check render when total diff rate is negative`() = runBlocking {
+        val report = Report("main", "assemble")
+        report.modulesSourceSizeReport = ModulesSourceSizeReport(
+            listOf(
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":woman",
+                    sizeInKb = 56,
+                    coverageRate = 14F,
+                    diffRate = 12F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":life",
+                    sizeInKb = 100,
+                    coverageRate = 23F,
+                    diffRate = 0F
+                ),
+                ModulesSourceSizeReport.ModuleSourceSize(
+                    path = ":freedom",
+                    sizeInKb = 112,
+                    coverageRate = 65F,
+                    diffRate = -14F
+                )
+            ),
+            totalSourceSizeInKb = 1024,
+            totalDiffRate = -25F
+        )
+
+        val renderTemplate = "%modules-source-size-metric%"
+        val stage = RenderModulesSourceSizeReportStage(report)
+        val result = stage.process(renderTemplate)
+
+        assertTrue {
+            result.contains("<tr>\n" +
+                    "                        <td colspan=\"2\">Total</td>\n" +
+                    "                        <td>1024kb</td>\n" +
+                    "                        <td>Σ</td>\n" +
+                    "                        <td>-25.0%</td>\n" +
+                    "                    </tr>")
+        }
+        assertTrue {
+            result.contains("labels: [\":woman\", \":life\", \":freedom\"]")
+        }
+        assertTrue {
+            result.contains("var _data = [56, 100, 112];")
+        }
+        assertTrue {
+            result.contains("<td>1</td>\n" +
+                    "    <td>:woman</td>\n" +
+                    "    <td>56kb</td>\n" +
+                    "    <td>14.0%</td>\n" +
+                    "    <td>+12.0%</td>")
+        }
+        assertTrue {
+            result.contains("<td>2</td>\n" +
+                    "    <td>:life</td>\n" +
+                    "    <td>100kb</td>\n" +
+                    "    <td>23.0%</td>\n" +
+                    "    <td>Equals</td>")
+        }
+        assertTrue {
+            result.contains(
+                "<td>3</td>\n" +
+                        "    <td>:freedom</td>\n" +
+                        "    <td>112kb</td>\n" +
+                        "    <td>65.0%</td>\n" +
+                        "    <td>-14.0%</td>"
+            )
+        }
     }
 
 }
