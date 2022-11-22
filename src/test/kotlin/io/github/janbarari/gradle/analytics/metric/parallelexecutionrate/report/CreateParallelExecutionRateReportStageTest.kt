d55301ce6aef3f3f835a374ec5911690d439786b
janbarari <mehdimjt@gmail.com>
2022-11-22 13:39:10 +0330

    Add parallel execution rate report tests

--- src/test/kotlin/io/github/janbarari/gradle/analytics/metric/parallelexecutionrate/report/CreateParallelExecutionRateReportStageTest.kt
+++ src/test/kotlin/io/github/janbarari/gradle/analytics/metric/parallelexecutionrate/report/CreateParallelExecutionRateReportStageTest.kt
@@ -22,18 +22,66 @@
  */
 package io.github.janbarari.gradle.analytics.metric.parallelexecutionrate.report
 
+import io.github.janbarari.gradle.analytics.domain.model.metric.BuildMetric
+import io.github.janbarari.gradle.analytics.domain.model.metric.ParallelExecutionRateMetric
+import io.github.janbarari.gradle.analytics.domain.model.report.Report
+import io.github.janbarari.gradle.analytics.metric.paralleexecutionrate.report.CreateParallelExecutionRateReportStage
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CreateParallelExecutionRateReportStageTest {
 
     @Test
     fun `check process() generates report when metric is not available`() = runBlocking {
+        val metrics = mutableListOf<BuildMetric>()
+        val stage = CreateParallelExecutionRateReportStage(metrics)
+        var report = Report("main", "assemble")
+        report = stage.process(report)
 
+        assertEquals(null, report.parallelExecutionRateReport)
+        assertEquals(null, report.parallelExecutionRateReport)
     }
 
     @Test
     fun `check process() generates report when metric is available`() = runBlocking {
+        val metrics = mutableListOf<BuildMetric>()
+        metrics.add(
+            BuildMetric(
+                branch = "main",
+                requestedTasks = listOf("assemble"),
+                createdAt = 1668836798265,
+                gitHeadCommitHash = UUID.randomUUID().toString(),
+                parallelExecutionRateMetric = ParallelExecutionRateMetric(35)
+            )
+        )
+
+        metrics.add(
+            BuildMetric(
+                branch = "main",
+                requestedTasks = listOf("assemble"),
+                createdAt = 1668936974389,
+                gitHeadCommitHash = UUID.randomUUID().toString(),
+                parallelExecutionRateMetric = ParallelExecutionRateMetric(45)
+            )
+        )
+
+
+        val stage = CreateParallelExecutionRateReportStage(metrics)
+        var report = Report("main", "assemble")
+        report = stage.process(report)
+
+        assertEquals(2, report.parallelExecutionRateReport!!.medianValues.size)
+        assertTrue {
+            report.parallelExecutionRateReport!!.medianValues[0].value == 35L &&
+                    report.parallelExecutionRateReport!!.medianValues[0].from == 1668836798265
+        }
+        assertTrue {
+            report.parallelExecutionRateReport!!.medianValues[1].value == 45L &&
+                    report.parallelExecutionRateReport!!.medianValues[1].from == 1668936974389
+        }
 
     }
 
